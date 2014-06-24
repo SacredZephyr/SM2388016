647e50105c55dca0923fe1bcbbe2d41b193948d9
Sergio R Montalvan <leonredgrave@live.com>
2014-06-24 13:42:12 -0700

    First Project: Hello World
    The first project done in class and redone at home.

---
+++ Hello_World/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: Sergio R. Montalvan
+ * Created on June 24, 2014, 8:17 AM
+ * Purpose : First Project
+ */
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
+
